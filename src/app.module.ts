4a14ed495368daae54f583e07fd9f222194d79ed
Marigen Rivero <marigen@gmail.com>
2024-09-06 12:13:41 +0200

    ✨ NEW FEATURE: login

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    AuthModule,
-    MongooseModule.forRoot('mongodb://localhost/:27017/mean-db'),
-  ],
+  imports: [AuthModule, MongooseModule.forRoot('mongodb://localhost/:27017')],
   controllers: [],
   providers: [],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,53 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const { password, ...userData } = createUserDto;
+
+      const newUser = new this.userModel({
+        password: bcrypt.hashSync(password, 10),
+        ...userData,
+      });
+
+      await newUser.save();
+      const { password: _, ...user } = newUser.toJSON();
+      return user;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} already exists!`);
+      }
+      throw new InternalServerErrorException('error servidor');
+    }
+  }
+
+  async login(loginDto: LoginDto) {
+    console.log({ loginDto });
+    const { email, password } = loginDto;
+    const user = await this.userModel.findOne({ email });
+    if (!user) {
+      throw new UnauthorizedException('user not found');
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('password incorrect');
+    }
+    const { password: _, ...res } = user.toJSON();
+    return { user: res, toke: 'ABC-123' };
   }
 
   findAll() {
